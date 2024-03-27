3005e0df67443bd2796c91d797a9b6731f8e8117
RobotX23 <RobotX23@yandex.ru>
2024-03-27 22:49:37 +0700

    4 test

---
+++ test_main.py
@@ -0,0 +1,52 @@
+import pytest
+from main import BooksCollector
+
+# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
+# обязательно указывать префикс Test
+
+
+
+class TestBooksCollector:
+
+
+    # пример теста:
+    # обязательно указывать префикс test_
+    # дальше идет название метода, который тестируем add_new_book_
+    # затем, что тестируем add_two_books - добавление двух книг
+    def test_add_new_book_add_two_books(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Что делать, если ваш кот хочет вас убить")
+        # проверяем, что добавилось именно две
+        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
+
+    # напиши свои тесты ниже
+    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    # проверка добавления жанра 2-х книг
+    def test_set_book_genre_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Что делать, если ваш кот хочет вас убить")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.set_book_genre("Что делать, если ваш кот хочет вас убить", "Детективы")
+
+        assert collector.get_book_genre("Гордость и предубеждение и зомби") == "Фантастика" and collector.get_book_genre("Что делать, если ваш кот хочет вас убить") == "Детективы"
+
+    def test_get_books_for_children_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Что делать, если ваш кот хочет вас убить")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.set_book_genre("Что делать, если ваш кот хочет вас убить", "Детективы")
+        assert collector.get_books_for_children() != []
+
+    def test_get_books_with_specific_genre_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гордость и предубеждение и зомби")
+        collector.add_new_book("Что делать, если ваш кот хочет вас убить")
+        collector.set_book_genre("Гордость и предубеждение и зомби", "Фантастика")
+        collector.set_book_genre("Что делать, если ваш кот хочет вас убить", "Детективы")
+        assert "Гордость и предубеждение и зомби" in collector.get_books_with_specific_genre("Фантастика")
